ec2c56e3183e9b390e53839ed6d7276259bca43e
JedwardMook <jedwardmook@yahoo.com>
2023-06-27 18:25:07 -0500

    tests complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+# your code goes here!env shell
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        seperate_word = sorted([letter for letter in self.word])
+        anagrams_array = []
+        for array in list:
+            if(seperate_word == sorted(array)):
+                anagrams_array.append(array)
+                print(anagrams_array)
+        if(len(anagrams_array) > 0):
+            return anagrams_array
+        else:
+            return []
